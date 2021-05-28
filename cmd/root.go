bc65f461bb9a7e76b0be8e73f922725560bbb9c0
Sean Cheng <blackhorseya@gmail.com>
2021-05-29 02:28:46 +0800

    feat: update config flag

--- cmd/root.go
+++ cmd/root.go
@@ -40,7 +40,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tap-exchangeratesapi.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "./configs/config.json", "config file (default is $HOME/.tap-exchangeratesapi.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
