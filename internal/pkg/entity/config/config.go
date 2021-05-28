575b3a57c0b404da37abb5a6cff47838b96b7a40
Sean Cheng <blackhorseya@gmail.com>
2021-05-29 02:28:18 +0800

    feat: added tag for viper snake case

---
+++ internal/pkg/entity/config/config.go
@@ -0,0 +1,8 @@
+package config
+
+// Config declare configuration of application
+type Config struct {
+	ApiKey    string `json:"api_key" mapstructure:"api_key"`
+	Base      string `json:"base" mapstructure:"base"`
+	StartDate string `json:"start_date" mapstructure:"start_date"`
+}
