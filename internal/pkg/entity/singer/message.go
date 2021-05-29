53f609edb3388fc0f926ee6396a9cb2ae7515e72
Sean Cheng <blackhorseya@gmail.com>
2021-05-30 03:27:47 +0800

    feat: added record message and new

--- internal/pkg/entity/singer/message.go
+++ internal/pkg/entity/singer/message.go
@@ -2,19 +2,36 @@ package singer
 
 // SchemaMessage declare schema message
 type SchemaMessage struct {
-	Type   string  `json:"type"`
-	Stream string  `json:"stream"`
-	Schema *Schema `json:"schema"`
+	Type          string   `json:"type"`
+	Stream        string   `json:"stream"`
+	Schema        *Schema  `json:"schema"`
 	KeyProperties []string `json:"key_properties"`
-	Selected bool `json:"selected"`
+	Selected      bool     `json:"selected"`
 }
 
 // NewSchemaMessage serve caller to create a SchemaMessage
 func NewSchemaMessage(stream string, schema *Schema, keyProperties []string) *SchemaMessage {
 	return &SchemaMessage{
-		Type:   "SCHEMA",
-		Stream: stream,
-		Schema: schema,
+		Type:          "SCHEMA",
+		Stream:        stream,
+		Schema:        schema,
 		KeyProperties: keyProperties,
+	}
+}
+
+// RecordMessage declare record message
+type RecordMessage struct {
+	Type          string  `json:"type"`
+	Stream        string  `json:"stream"`
+	Record        *Record `json:"record"`
+	TimeExtracted string  `json:"time_extracted"`
+}
+
+// NewRecordMessage serve caller to create a RecordMessage
+func NewRecordMessage(stream string, record *Record) *RecordMessage {
+	return &RecordMessage{
+		Type:   "RECORD",
+		Stream: stream,
+		Record: record,
 	}
 }
--- internal/pkg/entity/singer/message_test.go
+++ internal/pkg/entity/singer/message_test.go
@@ -10,6 +10,11 @@ var (
 		Type:          "SCHEMA",
 		Stream:        "exchange_rate",
 	}
+
+	recordMsg = &RecordMessage{
+		Type:          "RECORD",
+		Stream:        "exchange_rate",
+	}
 )
 
 func TestNewSchemaMessage(t *testing.T) {
@@ -33,6 +38,31 @@ func TestNewSchemaMessage(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewSchemaMessage(tt.args.stream, tt.args.schema, tt.args.keyProperties); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewSchemaMessage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNewRecordMessage(t *testing.T) {
+	type args struct {
+		stream string
+		record *Record
+	}
+	tests := []struct {
+		name string
+		args args
+		want *RecordMessage
+	}{
+		{
+			name: "exchange rate then success",
+			args: args{stream: "exchange_rate"},
+			want: recordMsg,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewRecordMessage(tt.args.stream, tt.args.record); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRecordMessage() = %v, want %v", got, tt.want)
 			}
 		})
 	}
