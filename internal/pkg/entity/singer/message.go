ae4e45156d1900e9ca765998cff3b36aa63e0d28
Sean Cheng <blackhorseya@gmail.com>
2021-05-30 02:24:00 +0800

    feat: update new schema message

--- internal/pkg/entity/singer/message.go
+++ internal/pkg/entity/singer/message.go
@@ -5,13 +5,16 @@ type SchemaMessage struct {
 	Type   string  `json:"type"`
 	Stream string  `json:"stream"`
 	Schema *Schema `json:"schema"`
+	KeyProperties []string `json:"key_properties"`
+	Selected bool `json:"selected"`
 }
 
 // NewSchemaMessage serve caller to create a SchemaMessage
-func NewSchemaMessage(stream string, schema *Schema) *SchemaMessage {
+func NewSchemaMessage(stream string, schema *Schema, keyProperties []string) *SchemaMessage {
 	return &SchemaMessage{
 		Type:   "SCHEMA",
 		Stream: stream,
 		Schema: schema,
+		KeyProperties: keyProperties,
 	}
 }
